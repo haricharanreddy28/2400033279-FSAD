da98d72a7e7f6e3d1b974bbb81dbb4957b75f90b
GOURAVKARUMUDI <gouravkarumudi6@gmail.com>
2026-03-04 21:42:30 +0530

    Skill 1 Git Version Control

--- GitVersionControl/main.java
+++ GitVersionControl/main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello Git");
-        System.out.println("Hello");
+        System.out.println("My name is K.Gourav");
+        System.out.println("I am a KL Student");
     }
 }
